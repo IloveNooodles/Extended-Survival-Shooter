4aeb392890d7af53b18c5dfa205e09890d24dbb8
IloveNooodles <mgarebaldhie80@gmail.com>
2023-04-07 01:07:04 +0700

    feat: main menu working

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,7 +1,15 @@
-﻿namespace Menu
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
 {
-    public class MainMenu
-    {
-        
-    }
+        public void NewGame()
+        {
+                SceneManager.LoadScene(1);
+        }
+
+        public void Exit()
+        {
+                Application.Quit();
+        }
 }
