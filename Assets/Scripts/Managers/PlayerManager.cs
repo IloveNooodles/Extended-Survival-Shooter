70dab31105dea4f87604893fa4f803f441b5d9d3
IloveNooodles <mgarebaldhie80@gmail.com>
2023-04-11 04:46:18 +0700

    feat: timer manager

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static string name;
+
+    private void Awake()
+    {
+        name = "Player";
+    }
+
+    private static void SetName(string newName)
+    {
+        name = newName;
+    }
+    
+}
---
+++ Assets/Scripts/Managers/TimerManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour
+{
+    public static float time;
+    private bool isActive;
+
+    private Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+        time = 0;
+        isActive = true;
+    }
+
+    public void stopTimer()
+    {
+        isActive = false;
+    }
+
+    public void startTimer()
+    {
+        isActive = true;
+    }
+    
+    private void Update()
+    {
+        if(!isActive) return;
+        time += Time.deltaTime;
+        TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+        text.text = "Time: " + timeSpan.ToString(@"mm\:ss\:fff");
+    }
+}
