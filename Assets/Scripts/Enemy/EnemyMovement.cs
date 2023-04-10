a8720f5d1d0b0e4924079dcc920d93f432a01468
firizky29 <13520095@std.stei.itb.ac.id>
2023-04-10 22:00:38 +0700

    feat: pet model, animation, material, movement (not finished)

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -6,11 +6,18 @@ public class EnemyMovement : MonoBehaviour
     PlayerHealth playerHealth;
     EnemyHealth enemyHealth;
     UnityEngine.AI.NavMeshAgent nav;
+    private Transform pet;
 
     void Awake()
     {
         //Cari game object with tag player
         player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        // Cari game object with tag pet
+        if (GameObject.FindGameObjectWithTag("Pet")!= null)
+        {
+            pet = GameObject.FindGameObjectWithTag("Pet").transform;
+        }
 
         //Mendapatkan componen reference
         playerHealth = player.GetComponent<PlayerHealth>();
@@ -25,6 +32,16 @@ public class EnemyMovement : MonoBehaviour
         if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
         {
             nav.SetDestination(player.position);
+            
+            if (pet != null)
+            {
+                // find the closest distance between pet and player
+                float distanceToPlayer = (player.position - transform.position).sqrMagnitude;
+                float distanceToPet = (pet.position - transform.position).sqrMagnitude;
+                
+                if(distanceToPlayer < distanceToPet)
+                    nav.SetDestination(player.position);
+            }
         }
         else //Stop moving
         {
