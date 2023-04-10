a3864c0b03e7d0450bb7d8ff6358de8f5e4f3714
IloveNooodles <mgarebaldhie80@gmail.com>
2023-04-11 00:03:34 +0700

    feat: merge from main

--- Assets/Scripts/CutScene/FPS/SecondCityCutScene.cs
+++ Assets/Scripts/CutScene/FPS/SecondCityCutScene.cs
@@ -8,6 +8,17 @@ public class SecondCityCutScene : MonoBehaviour
     public CutSceneManager cutSceneManager;
     PlayableDirector playableDirector;
     GameObject cutSceneClip;
+    public GameObject wizards;
+    public GameObject groundEnemy;
+    bool isWizardSpawned = false;
+
+    void FixedUpdate(){
+        if(isWizardSpawned){
+            if(wizards.transform.position.y >= 6){
+                wizards.transform.position -= new Vector3(0, 10f, 0) * Time.deltaTime;
+            }
+        }
+    }
 
     void Start(){
         cutSceneClip = gameObject.transform.GetChild(0).gameObject;
@@ -25,10 +36,19 @@ public class SecondCityCutScene : MonoBehaviour
         cutSceneManager.startCutScene();
         cutSceneClip.SetActive(true);
         playableDirector.Play();
+        StartCoroutine(spawnEnemy());
     }
 
     public void endCutScene(){
         gameObject.SetActive(false);
         cutSceneManager.endCutScene();
+    }
+
+    IEnumerator spawnEnemy(){
+        yield return new WaitForSeconds(13f);
+        wizards.SetActive(true);
+        isWizardSpawned = true;
+        yield return new WaitForSeconds(8f);
+        groundEnemy.SetActive(true);
     }
 }
