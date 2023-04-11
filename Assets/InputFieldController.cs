1a7a1b706634deabd5299a66c70601a8015f3e99
Fayza <fayzanadia@gmail.com>
2023-04-11 10:08:59 +0700

    feat: setting UI

---
+++ Assets/InputFieldController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputFieldController : MonoBehaviour
+{
+    public TMP_InputField inputField;
+
+    // on select function
+    public void OnSelect()
+    {
+        // change border width
+
+
+    }
+}
