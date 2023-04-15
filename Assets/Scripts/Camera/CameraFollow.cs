4e72be16e1c110e4daff034aa4dfac09c5177582
firizky29 <13520095@std.stei.itb.ac.id>
2023-04-15 12:46:59 +0700

    feat: shop ui fix and camera follow fix

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class CameraFollow : MonoBehaviour
 {
     private static CameraFollow instance;
@@ -14,18 +16,34 @@ public class CameraFollow : MonoBehaviour
         target = player.transform;
     }
 
+
     private void Start()
     {
         //Mendapatkan offset antara target dan camera
-        offset = transform.position - target.position;
+        // offset = transform.position - target.position;
+        // offset = new Vector3(0, 0, -10);
+
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            offset = new Vector3(6.54f, 6.71f, -10.22f);
+        } else if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            offset = new Vector3(1.00f, 15.00f, -22.00f);
+        }
     }
     
+
     private void FixedUpdate()
     {
+
+        // Debug.Log(offset);
         //Menapatkan posisi untuk camera
         Vector3 targetCamPos = target.position + offset;
+        
+        // Debug.Log(targetCamPos + ", " + target.position + ", from" + transform.position);
 
         //set posisi camera dengan smoothing
         transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
     }
+    
 }
